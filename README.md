580ab9af69187906a35643f52fe7c969e7f6fb02
Daniel CAUNE <daniel.caune@gmail.com>
2019-08-29 09:45:05 +0700

    Complete first waypoint

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ For example, the animation of the hero Marco Rossi (Metal Slug), running with a 
 
 ![Metal Slug Sprite Running with a Gun](metal_slug_sprites_running_with_gun.png)
 
-## Sprite Sheets
+## Sprite Sheet
 
 It is not uncommon for games to have tens to hundreds of sprites. Loading each of these as an individual image would consume a lot of memory and processing power. To help manage sprites and avoid using so many images, many games use **sprite sheets** (also known as **image sprites**).
 
@@ -40,7 +40,7 @@ For example:
 
 ## Sprite Mask
 
-The mask of a sprite defines the 2D shape of the sprite. For example, the sprite sheet [`metal_slug_sprite_standing_stance_large.png`](metal_slug_sprite_standing_stance_large.png) contains the 3 following sprites:
+The mask of a sprite defines the 2D shape of the sprite. For example, the sprite sheet [`metal_slug_sprite_standing_stance.png`](metal_slug_sprite_standing_stance_large.png) contains the 3 following sprites:
 
 ![Metal Slug Standing Stance](metal_slug_sprite_standing_stance_large.png)
 
@@ -48,7 +48,7 @@ The masks of these sprites are respectively:
 
 ![](metal_slug_sprite_detection_coloring.png)
 
-## Optimized Sprite Sheets
+## Optimized Sprite Sheet
 
 Sprites could be evenly placed in the sprite sheet according to their bounding box. The disadvantage is that this method of placement wastes a lot of memory because of all the additional transparency of each sprite, especially when sprites have different width and height.
 
@@ -62,36 +62,88 @@ This space optimization is even more efficient when some big sprites have concav
 | ----------------------- | -------------------------------------------- | ------------------------------------------------- |
 | ![Islands](islands.png) | ![Islands Sprite Sheet](islands_sprites.png) | ![Islands Sprite Masks](islands_sprite_masks.png) |
 
+# Waypoint: Find the Most Common Color in an Image
+
+Write a function `find_most_common_color` that takes an argument `image` (a [`Image`](https://pillow.readthedocs.io/en/stable/reference/Image.html) object) and that returns the pixel color that is the most used in this image.
+
+The data type of the value returned depends on the [image's mode](https://pillow.readthedocs.io/en/stable/handbook/concepts.html#concept-modes):
+
+- an integer if the mode is grayscale;
+- a tuple `(red, green, blue)` of integers if the mode is `RGB`;
+- a tuple `(red, green, blue, alpha)` of integers if the mode is `RGBA`
+
+For example:
+
+```python
+# JPEG image
+>>> image = Image.open('islands.jpg')
+>>> image.mode
+'RGB'
+>>> find_most_common_color(image)
+(0, 221, 204)
+# PNG image
+>>> image = Image.open('metal_slug_sprite_standing_stance_large.png')
+>>> image.mode
+'RGBA'
+>>> find_most_common_color(image)
+(0, 0, 0, 0)
+# Grayscale image
+>>> image = image.convert('L')
+>>> image.mode
+'L'
+>>> find_most_common_color(image)
+152
+```
+
+_Note 1: Python is an interpreted, high-level, general-purpose programming language. Pure Python code is not great at processing large amount of data. However, Python comes with a large number of libraries written in [`C`](<https://en.wikipedia.org/wiki/C_(programming_language)>) that run very fast: [`Pillow`](https://en.wikipedia.org/wiki/Python_Imaging_Library) for image processing, [`NumPy`](https://en.wikipedia.org/wiki/NumPy) for large multi-dimensional arrays and matrices, etc. You SHOULD intensively use these libraries that extend Python and that allow your Python application to be fast._
+
+_Note 2: You SHOULD [measure the execution time](https://docs.python.org/3.7/library/timeit.html) of your code to check whether it's reasonably fast or if there is space for improvement. For instance:_
+
+```python
+>>> import timeit
+>>> image = Image.open('islands.jpg')
+>>> timeit.timeit(stmt=lambda: find_most_common_color(image), number=1)
+1.6871624910000023
+```
+
 # Waypoint: Write a class `Sprite`
 
 Write a class `Sprite` that contains the following attributes:
 
 - `top_left`: A 2D point that indicates the top-left position of the bounding box of the shape in the image;
-
 - `bottom_right`: A 2D point that indicates the bottom-most position of the bounding box of the shape in the image;
-
 - `mask_color`: An integer representing the RGB value of this sprite in the mask image.
 
 # Waypoint: Write a class `SpriteSheet`
 
+Write a class `SpriteSheet` which constructor accepts an argument `image` that corresponds to either:
+
+- the name and path (a string) that references an image file in the local file system;
+- a [`pathlib.Path`](https://docs.python.org/3/library/pathlib.html#pathlib.Path) object that references an image file in the local file system ;
+- a [file object](https://docs.python.org/3/glossary.html#term-file-object) that MUST implement [`read()`](https://docs.python.org/3/library/io.html#io.IOBase.read), [`seek()`](https://docs.python.org/3/library/io.html#io.IOBase.seek), and [`tell()`](https://docs.python.org/3/library/io.html#io.IOBase.tell) methods, and be opened in binary mode;
+- a [`Image`](https://pillow.readthedocs.io/en/stable/reference/Image.html) object.
+
+This constructor also accepts an optional argument `transparent_color` that identifies the transparent color of the image. The type of `transparent_color` argument depends on the images' mode:
+
+- an integer if the mode is grayscale;
+- a tuple `(red, green, blue)` of integers if the mode is `RGB`;
+- a tuple `(red, green, blue, alpha)` of integers if the mode is `RGBA`. The `alpha` element is optional. If not defined, while the image mode is `RGBA`, the constructor considers the `alpha` element to be `255`.
+
 # Waypoint: Detect the Sprites in a Sprite Sheet Picture
 
-We would like to detect all the sprites packed in a single picture. For example, providing the following image [`metal_slug_standing_stance.png`](./metal_slug_standing_stance.png):
+We would like to detect all the sprites packed in a single picture.
 
-Write a function `detect_sprites` that takes an argument `image` (an object [`Image`](https://pillow.readthedocs.io/en/stable/reference/Image.html)) and that returns an object `SpriteSheet`.
+Write a function `detect_sprites` that takes an argument `image` (a [`Image`](https://pillow.readthedocs.io/en/stable/reference/Image.html) object) and that returns an object `SpriteSheet`.
 
 An object `SpriteSheet`
 
 - A list of objects `Sprite` corresponding to all the sprites that have been detected;
-
 - An object [`Image`](https://pillow.readthedocs.io/en/stable/reference/Image.html), corresponding to the masks of the detected sprites.
 
 An object `Sprite` contains the following attributes:
 
 - `top_left`: A 2D point that indicates the top-left position of the bounding box of the shape in the image;
-
 - `bottom_right`: A 2D point that indicates the bottom-most position of the bounding box of the shape in the image;
-
 - `mask_color`: An integer representing the RGB value of this sprite in the mask image.
 
 The function `detect_shapes` accepts an optional argument `transparent_color` (an integer if the image format is grayscale, or a tuple `(red, green, blue)` if the image format is `RGB`) that identifies the transparent color of the image. The function ignores any pixels of the image with this color.
@@ -105,7 +157,9 @@ If this argument `transparent_color` is not passed, the function determines the 
 For example:
 
 ```python
->>> sprite_sheet = detect_sprites('./metal_slug_sprite_standing_stance.png')
+>>> from PIL import Image
+>>> image = Image.open('./metal_slug_sprite_standing_stance.png')
+>>> sprite_sheet = detect_sprites(image)
 >>> len(shapes)
 3
 >>> first_shape = shapes[0]
---
+++ ri/sprite_sheet.py
@@ -0,0 +1,354 @@
+# Copyright (C) 2019 Intek Institute.  All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Intek Institute or one of its subsidiaries.  You shall not disclose
+# this confidential information and shall use it only in accordance
+# with the terms of the license agreement or other applicable
+# agreement you entered into with Intek Institute.
+#
+# INTEK INSTITUTE MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
+# SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE, OR NON-INFRINGEMENT.  INTEK
+# INSTITUTE SHALL NOT BE LIABLE FOR ANY LOSSES OR DAMAGES SUFFERED BY
+# LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS
+# SOFTWARE OR ITS DERIVATIVES.
+
+import argparse
+import collections
+import datetime
+import random
+
+from PIL import Image
+import numpy
+
+
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+
+class Sprite:
+    def __init__(self, top_left, bottom_right, mask_color):
+        if not isinstance(top_left, Point) or not isinstance(bottom_right, Point):
+            raise ValueError('Invalid arguments')
+
+        self.__top_left = top_left
+        self.__bottom_right = bottom_right
+        self.__mask_color = mask_color
+
+    @property
+    def bottom_right(self):
+        return self.__bottom_right
+
+    @property
+    def mask_color(self):
+        return self.__mask_color
+
+    @property
+    def top_left(self):
+        return self.__top_left
+
+
+class SpriteSheet:
+    # +---+---+---+
+    # | ? | ? | ? |
+    # +---+---+---+
+    # | ? | X |
+    # +---+---+
+    NEIGHBOR_PIXEL_RELATIVE_COORDINATES = ((-1, -1), (0, -1), (1, -1), (-1, 0))
+
+    def __init__(self, image, transparent_color=None):
+        self.__image = image
+        self.__transparent_color = transparent_color
+
+        # Dictionary of sprites connected to each other.
+        self.__sprites_links = {}
+
+    def __link_sprites(self, id1, id2):
+        """
+        Link the two sprites, and all the other sprites connected to them.
+
+        :param sprites_links:
+        :param id1:
+        :param id2:
+        :return:
+        """
+        sprite_indices = self.__sprites_links[id1] + self.__sprites_links[id2]
+        for sprite_index in sprite_indices:
+            self.__sprites_links[sprite_index] = sprite_indices
+
+    def detect_sprites(self, transparent_color=None):
+        # Determine the transparent color if not specified by the caller.
+        if transparent_color is None:
+            transparent_color = self.detect_most_common_color()
+
+        # Convert the image into an array for faster access.
+        image_pixels = numpy.asarray(self.__image)
+
+        # Build the mask of the image used to store the sprite index for each
+        # pixel of the given image.  The image mask is initially empty.
+        image_width, image_height = self.__image.size
+        image_mask = numpy.asarray([[0] * image_width] * image_height)
+
+        # Start sprite index with `1`; the value `0` for the sprite index of a
+        # pixel in the image mask means that this pixel doesn't belongs to any
+        # sprite (e.g., this is a transparent pixel).
+        sprite_index = 1
+
+        for y in range(image_height):
+            for x in range(image_width):
+                if tuple(image_pixels[y][x]) != transparent_color:  # @todo: convert `transparent_color` to a numpy.ndarray
+
+                    pixel_sprite_index = 0
+                    for dx, dy in SpriteSheet.NEIGHBOR_PIXEL_RELATIVE_COORDINATES:
+                        # Check whether a neighbor pixel belongs to a sprite.
+                        if 0 <= x + dx < image_width and y + dy >= 0 and image_mask[y + dy][x + dx] > 0:
+                            # If the current pixel has been already associated to a sprite, check
+                            # whether the neighbor pixel belongs to this same sprite, and if not,
+                            # merge those two sprites together.
+                            if pixel_sprite_index and pixel_sprite_index != image_mask[y + dy][x + dx] \
+                                    and image_mask[y + dy][x + dx] not in sprites_links[pixel_sprite_index]:
+                                self.__link_sprites(pixel_sprite_index, image_mask[y + dy][x + dx])
+
+                            pixel_sprite_index = image_mask[y + dy][x + dx]
+
+                    image_mask[y][x] = pixel_sprite_index
+
+                    if pixel_sprite_index == 0:
+                        image_mask[y][x] = sprite_index
+                        sprites_links[sprite_index] = [sprite_index]
+                        sprite_index += 1
+
+        return image_mask, sprites_links
+
+    def detect_most_common_color(self):
+        """
+        Return the color that is the most common in the given image.
+
+
+        :param image: A `PIL.Image` object.
+
+
+        :return: A tuple or an integer representing the color that is the most
+            common in the given image.
+        """
+        pixels = numpy.asarray(self.__image)
+
+        # Check whether the value of pixels is composed of multiple components
+        # (e.g., RGB, RGBA, CMYK, YCbCr, etc.)
+        is_multiple_components_color = len(pixels[0][0]) > 1
+
+        # Count the number of times a pixel value is common in the given image.
+        colors_count = {}
+        for y in range(len(pixels)):
+            for x in range(len(pixels[y])):
+                # For image defined with multiple component color pixel, convert the
+                # numpy data type (not hashable) representing the pixel value to its
+                # tuple version (hashable).  This is a lot faster than converting the
+                # pixel value to an integer with:
+                #
+                #     sum([c << i for i, c in enumerate(pixels[y][x])])
+                color = tuple(pixels[y][x]) if is_multiple_components_color else pixels[y][x]
+                colors_count[color] = colors_count.get(color, 0) + 1
+
+        # Sort pixel value usages by decreasing order to retrieve the most common
+        # pixel value.
+        sorted_colors_count = sorted(colors_count.items(), key=lambda x: x[1], reverse=True)
+        most_common_color, most_common_color_count = sorted_colors_count[0]
+
+        return most_common_color
+
+
+
+
+
+def main():
+    arguments = parse_arguments()
+
+    image = Image.open(arguments.file_path_name)
+    detect_sprites(image)
+
+
+def parse_arguments():
+    """
+    Convert argument strings to objects and assign them as attributes of
+    the namespace.
+
+
+    @return: an instance ``argparse.Namespace`` corresponding to the
+        populated namespace.
+    """
+    parser = argparse.ArgumentParser(description='Sprite Detection')
+    parser.add_argument(
+        '-f', '--file',
+        dest='file_path_name',
+        metavar='FILE',
+        required=True,
+        help='specify the absolute name and path of the sprite sheet image file.')
+
+    return parser.parse_args()
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+def build_sprites_mask_image(file_path_name, image_mask, sprites_links, unified=False, background_color=None):
+    if not background_color:
+        background_color = (255, 255, 255)
+
+    sprites_color = dict()
+
+    if unified:
+        for sprites_indices in sprites_links.values():
+            primary_sprite_index = sprites_indices[0]
+            if primary_sprite_index not in sprites_color:
+                sprites_color[primary_sprite_index] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        pixels = numpy.asarray(
+            [[sprites_color[sprites_links[c][0]] if c else (255, 255, 255) for c in row] for row in image_mask],
+            dtype=numpy.uint8)
+
+    else:
+        sprites_color[0] = (255, 255, 255)
+
+        for k in sprites_links.keys():
+            sprites_color[k] = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+        pixels = numpy.asarray(
+            [[sprites_color[c] for c in row] for row in image_mask],
+            dtype=numpy.uint8)
+
+    Image.fromarray(pixels, 'RGB').save(file_path_name)
+
+
+#
+#
+# image = Image.open('/Users/dcaune/Devel/intek-mission-sprite_detection/metal_slug_sprite_sheet.png')
+# image_mask, sprites_links = SpriteSheet.detect_sprites(image)
+# build_sprites_mask_image('/Users/dcaune/Downloads/metal_slug_sprite_sheet_mask.png', image_mask, sprites_links)
+#
+#
+# sprite_sheet = SpriteSheet(image)
+# start_time = datetime.datetime.now()
+# image_mask, sprites_links = sprite_sheet.detect_sprites()
+# end_time = datetime.datetime.now()
+# execution_time = end_time - start_time
+#
+# execution_time
+# sprite_sheet.sprite_merging_time
+# execution_time - sprite_sheet.sprite_merging_time
+#
+# build_sprites_mask_image('/Users/dcaune/Devel/intek-mission-sprite_detection/islands_split_sprites_mask_.png', image_mask, sprites_links)
+# build_sprites_mask_image('/Users/dcaune/Devel/intek-mission-sprite_detection/islands_unified_sprites_mask_.png', image_mask, sprites_links, unified=True)
+#
+#
+# image = Image.open('/Users/dcaune/Devel/intek-mission-sprite_detection/qr_code.png')
+# image_mask, sprites_links = SpriteSheet.detect_sprites(image)
+#
+# build_sprites_mask_image('/Users/dcaune/Devel/intek-mission-sprite_detection/qr_code_decomposed_mask.png', image_mask, sprites_links)
+# build_sprites_mask_image('/Users/dcaune/Devel/intek-mission-sprite_detection/qr_code_unified_mask.png', image_mask, sprites_links, unified=True)
+
+
+def find_most_common_color(image):
+    """
+    Return the color that is the most common in the given image.
+
+
+    :param image: A `PIL.Image` object.
+
+
+    :return: An integer or a tuple of integers (one for each band red, green,
+        blue, and possibly alpha) representing the color that is the most
+        common in the given image.
+    """
+    if image.mode not in ('L', 'RGB', 'RGBA'):
+        raise ValueError(f"'The image mode '{image.mode}' is not supported")
+
+    if image.mode == 'L':
+        # Retrieve the list of colors used in this image.
+        #
+        # @note: Because this method is limited to a maximum number of colors
+        #     (256), this method can only be used for non-RGB image (grayscale).
+        colors_count = image.getcolors()
+
+        # Sort pixel value usages by decreasing order to retrieve the most common
+        # pixel value.
+        sorted_colors_count = sorted(colors_count, key=lambda color_count: color_count[0], reverse=True)
+        most_common_color_count, most_common_color = sorted_colors_count[0]
+
+    else:
+        # Count the number of times each unique pixel value is used in the
+        # image.
+        #
+        # The numpy array of a PIL image, composed of multiple bands, is a 3D
+        # array: an array of rows of this image, a sub-array of columns for this
+        # row, and a sub-array of the band values of the pixel for this column
+        # and this row.
+        #
+        # We need to flatten this array to a 2D array: an array of band values
+        # of each pixel of this image.
+        #
+        # @note: We could have used the following similar code, but it's 30%
+        #    slower (more likely the time to reshape the initial array).
+        #
+        #    ```python
+        #    image_width, image_height = image.size
+        #    flatten_colors = numpy.asarray(image) \
+        #        .flatten() \
+        #        .reshape((image_width * image_height, len(image.getbands())))
+        #    pixel_counter = collections.Counter(zip(*flatten_colors))
+        #    ```
+
+        # Split this image into individual bands; for example, splitting an "RGB"
+        # image creates three new images each containing a copy of one of the
+        # original bands (red, green, blue). Then build a list of numpy arrays of
+        # these image bands with flatten values.
+        color_channels = [
+            numpy.asarray(image_band).flatten()
+            for image_band in image.split()]
+
+        # Recompose pixel colors with its respective components taken from each
+        # image band.
+        flatten_colors = zip(*color_channels)
+
+        # Count the number of times each unique color is used in the image and
+        # retrieve the most common color.
+        pixel_counter = collections.Counter(flatten_colors)
+
+        most_common_color_counts = pixel_counter.most_common(1)
+        most_common_color, most_common_color_count = most_common_color_counts[0]
+
+    return most_common_color
+
+
+
+image = Image.open('/Users/dcaune/Devel/intek-mission-sprite_detection/metal_slug_sprite_standing_stance_large.png')
+find_most_common_color(image)
+#
+#
+# image = Image.open('/Users/dcaune/Devel/intek-mission-sprite_detection/islands.jpg')
+# image = image.convert('L')
+# find_most_common_color(image)
+#
+#
+# def test(image_file_path_name):
+#     image = Image.open(image_file_path_name)
+#     find_most_common_color(image)
+#
+#
+# image.width
+# image.height
+# import timeit
+# image = Image.open('/Users/dcaune/Devel/intek-mission-sprite_detection/metal_slug_sprite_standing_stance_large.png')
+# timeit.timeit(stmt=lambda: find_most_common_color(image), number=1)
