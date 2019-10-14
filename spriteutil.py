9c2da41de37e286b588d808b9b22f0956d66e161
longlamduc <longlamduc@gmail.com>
2019-10-14 13:19:38 +0700

    Set class attribute to private

--- spriteutil.py
+++ spriteutil.py
@@ -14,28 +14,28 @@ class Sprite():
             raise ValueError('Invalid coordinates')
         if args[1] > args[3] or args[2] > args[4]:
             raise ValueError('Invalid coordinates')
-        self.label = args[0]
-        self.x1 = args[1]
-        self.y1 = args[2]
-        self.x2 = args[3]
-        self.y2 = args[4]
+        self.__label = args[0]
+        self.__x1 = args[1]
+        self.__y1 = args[2]
+        self.__x2 = args[3]
+        self.__y2 = args[4]
 
     @property
     def label(self):
-        return self.label 
+        return self.__label 
 
     @property
     def top_left(self):
-        return (self.x1, self.y1)
+        return (self.__x1, self.__y1)
 
     @property
     def bottom_right(self):
-        return (self.x2, self.y2)
+        return (self.__x2, self.__y2)
 
     @property
     def width(self) :
-        return self.x2 - self.x1 + 1
+        return self.__x2 - self.__x1 + 1
 
     @property
     def height(self):
-        return self.y2 -  self.y1 + 1
+        return self.__y2 -  self.__y1 + 1
