31963ed727935d60f2ee7ed1fe696a0cf119e8a4
longlamduc <longlamduc@gmail.com>
2019-10-14 16:31:45 +0700

    Add mask for sprite

--- spriteutil.py
+++ spriteutil.py
@@ -4,6 +4,7 @@ from PIL import Image
 import numpy as np
 import sys
 import pprint
+import random
 
 def find_most_common_color(img):
     colors = img.getcolors(maxcolors=1000000)
@@ -79,7 +80,7 @@ def find_sprites(image, background_color=None):
     for row_idx, row in enumerate(lst_pixel):
         for col_idx, point in enumerate(row):   
             if not is_background(point, background_color) and not checked[row_idx][col_idx]: 
-                print(row_idx, col_idx)
+                # print(row_idx, col_idx)
                 label += 1
                 checked[row_idx][col_idx] = True
                 find_whole_sprite(label_map, lst_pixel, checked, row_idx, col_idx, label, background_color)
@@ -97,29 +98,62 @@ def find_sprites(image, background_color=None):
     # print(*label_map, sep='\n')
     return (sprites, label_map)
 
+
+def create_sprite_labels_image(sprites, label_map, background_color=None):
+    if not background_color:
+        background_color = (255, 255, 255)
+    if len(background_color) == 3:
+        mode = 'RGB'
+    else: 
+        mode = 'RGBA'
+    mask = Image.new(mode, (len(label_map[0]), len(label_map)), background_color)
+    sprite_colors = {'0': background_color}
+    for label in sprites.keys():
+        while True:
+            red = random.randint(0, 255)
+            green = random.randint(0, 255)
+            blue = random.randint(0, 255)
+            if not (red, green, blue) in [sprite_colors[key] for key in sprite_colors.keys()]:
+                sprite_colors[label] = (red, green, blue, 255) if mode == 'RGBA' else (red, green, blue)
+                break 
+    print(mask.size)
+    print(len(label_map[0]), len(label_map))
+    width, height = mask.size
+    for x in range(width):
+        for y in range(height):
+            if label_map[y][x] != 0:
+                mask.putpixel((x, y), sprite_colors[label_map[y][x]])
+    for label in sprites.keys():
+        sprite = sprites[label]
+        for x in range(sprite.top_left[0], sprite.bottom_right[0] + 1):
+            mask.putpixel((sprite.top_left[1], x), sprite_colors[sprite.label])
+        for x in range(sprite.top_left[1], sprite.bottom_right[1] + 1):
+            mask.putpixel((x, sprite.top_left[0]), sprite_colors[sprite.label])
+        for x in range(sprite.top_left[1], sprite.bottom_right[1] + 1):
+            mask.putpixel((x, sprite.bottom_right[0]), sprite_colors[sprite.label])
+        for x in range(sprite.top_left[0], sprite.bottom_right[0] + 1):
+            mask.putpixel((sprite.bottom_right[1], x), sprite_colors[sprite.label])
+    return mask
+
+
 # test
-image = Image.open('optimized_sprite_sheet.png')
+image = Image.open('metal_slug_sprite_sheet_large.png')
+print(image.size)
 # image = Image.open('islands.png')
 print(find_most_common_color(image))
 # sprites, label_map = find_sprites(image, background_color=(255, 255, 255))  # meta_slug
 # sprites, label_map = find_sprites(image, background_color=(0, 221, 204, 255))  # islands
+
 sprites, label_map = find_sprites(image)
-print(image.mode)
-for label, sprite in sprites.items():
-    print(f"Sprite ({label}): [{sprite.top_left}, {sprite.bottom_right}] {sprite.width}x{sprite.height}")
-    cropped_example = image.crop((sprite.top_left[1], sprite.top_left[0], sprite.bottom_right[1], sprite.bottom_right[0]))
-    cropped_example.show()
+# print(image.mode)
+# for label, sprite in sprites.items():
+#     print(f"Sprite ({label}): [{sprite.top_left}, {sprite.bottom_right}] {sprite.width}x{sprite.height}")
+#     cropped_example = image.crop((sprite.top_left[1], sprite.top_left[0], sprite.bottom_right[1], sprite.bottom_right[0]))
+#     cropped_example.show()
 # 
+
 # pprint.pprint(label_map, width=120)
 
-
-# img = Image.open('islands.png')
-# img = img.convert('L')
-# a = np.asarray(img)
-# b = Image.fromarray(a)
-# b.save('test.png')
-# print(img.mode)
-# print(a)
-# print(tuple(a[800][700]))
-# for x in a[800][700]:
-#     print(x)
+sprite_label_image = create_sprite_labels_image(sprites, label_map)
+# sprite_label_image = create_sprite_labels_image({}, {})
+sprite_label_image.save('test.png')
